3befbe85a66decab8ff3b97bcc30cd7994a0a6fe
Saeed-Altout <saeedaltoutpro@gmail.com>
2025-12-28 15:27:54 +0300

    chore: initial clerk for authentication

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+export default async function DashboardPage() {
+  const { isAuthenticated } = await auth();
+
+  if (!isAuthenticated) {
+    redirect("/sign-in");
+  }
+  return <div>dashboard protected</div>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -23,12 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("antialiased", poppins.className)}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
